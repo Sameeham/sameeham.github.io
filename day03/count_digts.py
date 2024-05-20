ce77eb43a2b5219200d7a14a8b32abd1bc0bd047
Sameeha Mittwali <Sameeha Mittwali>
2024-05-20 23:15:14 +0300

    seperating the display function

---
+++ day03/count_digts.py
@@ -0,0 +1,11 @@
+def coun_diftd(text):
+    # counter =[0,0,0,0,0,0,0,0,0,0]
+    counter =[0]*10
+    for ch in text:
+        print(ch)
+    print(text)
+    return counter 
+
+def main():
+    text="12341"
+    digits= count_digits(text)
---
+++ day03/rectangle_in_functions.py
@@ -0,0 +1,11 @@
+def display(area):
+    print(f"The area is {area}")
+
+def main():
+    length = input ("Length:")
+    width = input ("width:") 
+
+    area = int(length) * int(width) 
+    print(f"The area is {area}")
+    
+main()
